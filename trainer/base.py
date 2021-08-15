c2633c6702e624697ba4ea38ae1c0e1659f484be
Han Xiao <han.xiao@jina.ai>
2021-08-15 08:32:38 +0200

    refactor: change fit signature

--- trainer/base.py
+++ trainer/base.py
@@ -5,27 +5,27 @@ from typing import Optional, TypeVar, Union
 from jina import DocumentArray
 from jina.types.arrays.memmap import DocumentArrayMemmap
 
-DNN = TypeVar('DNN')
+AnyDNN = TypeVar('AnyDNN')
 
 
 class BaseTrainer(ABC):
     @abc.abstractmethod
     def __init__(
         self,
-        base_model: Optional[DNN] = None,
+        base_model: Optional[AnyDNN] = None,
         architecture: Optional[str] = None,
-        head_model: Union[DNN, str, None] = None,
+        head_model: Union[AnyDNN, str, None] = None,
         **kwargs
     ):
         ...
 
     @property
     @abc.abstractmethod
-    def base_model(self) -> DNN:
+    def base_model(self) -> AnyDNN:
         ...
 
     @base_model.setter
-    def base_model(self, val: DNN):
+    def base_model(self, val: AnyDNN):
         ...
 
     @property
@@ -39,15 +39,15 @@ class BaseTrainer(ABC):
 
     @property
     @abc.abstractmethod
-    def head_model(self) -> DNN:
+    def head_model(self) -> AnyDNN:
         ...
 
     @head_model.setter
-    def head_model(self, val: Union[str, DNN]):
+    def head_model(self, val: Union[str, AnyDNN]):
         ...
 
     @abc.abstractmethod
     def fit(
         self, doc_array: Union[DocumentArray, DocumentArrayMemmap], **kwargs
-    ) -> None:
+    ) -> AnyDNN:
         ...
