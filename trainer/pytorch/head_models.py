a45a48991835ce4f6dc3736711f9317ab0796bab
bwanglzu <bo.wang@jina.ai>
2021-08-17 13:54:19 +0200

    feat: add hat layer

--- trainer/pytorch/head_models.py
+++ trainer/pytorch/head_models.py
@@ -1,5 +1,28 @@
-import torch
+import abc
+
 import torch.nn as nn
 import torch.nn.functional as F
 
 
+class HeadLayer(nn.Module):
+    recommended_loss: str  #: the recommended loss function to be used when equipping this layer to base model
+
+    @abc.abstractmethod
+    def call(self, inputs, **kwargs):
+        ...
+
+
+class PairwiseHeadLayer(nn.Module):
+    @abc.abstractmethod
+    def call(self, lvalue, rvalue, **kwargs):
+        ...
+
+
+class CosineLayer(PairwiseHeadLayer):
+    recommended_loss = 'mse'
+
+    def call(self, lvalue, rvalue):
+        normalize_a = F.normalize(lvalue, p=2, dim=1)
+        normalize_b = F.normalize(rvalue, p=2, dim=1)
+        cos_similarity = nn.CosineSimilarity(dim=1)
+        return cos_similarity(normalize_a, normalize_b)
