5181842d599f8e474d70353c82d1065d56d541f4
bwanglzu <bo.wang@jina.ai>
2021-08-19 15:10:19 +0200

    feat: add iterable dataset

--- trainer/pytorch/dataset.py
+++ trainer/pytorch/dataset.py
@@ -1,29 +1,36 @@
-from typing import Union
+import itertools
+from typing import Union, Iterable
 
 import numpy as np
-from jina import DocumentArray
+from jina import Document, DocumentArray
 from jina.types.arrays.memmap import DocumentArrayMemmap
 from torch.utils.data import Dataset
 
 
 class JinaSiameseDataset(Dataset):
-
     def __init__(
-            self,
-            inputs: Union[DocumentArray, DocumentArrayMemmap],
+        self,
+        inputs: Union[Iterable[Document], DocumentArray, DocumentArrayMemmap],
     ):
-        self._pairs = []
-        for doc in inputs:
+        self._inputs = inputs
+        self._pairs = set()
+        for doc in self._inputs:
             for match in doc.matches:
-                self._pairs.append(
-                    (
-                        (doc.blob.astype(np.float32), match.blob.astype(np.float32)),
-                        np.float32(match.tags['trainer']['label']),
-                    )
-                )
+                self._pairs.add((doc.id, match.id))
 
     def __len__(self):
         return len(self._pairs)
 
-    def __getitem__(self, index):
-        return self._pairs[index]
+    def _parse_da(self):
+        for (doc_id, match_id) in self._pairs:
+            doc = self._inputs[doc_id]
+            match = doc.matches[match_id]
+            yield doc.blob.astype(np.float32), match.blob.astype(
+                np.float32
+            ), np.float32(match.tags['trainer']['label'])
+
+    def _stream(self):
+        return itertools.cycle(self._parse_da())
+
+    def __iter__(self):
+        return self._stream()
