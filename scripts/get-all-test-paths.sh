945bd6523abaf6b5bf905373e0f7c07bd1a70197
Han Xiao <han.xiao@jina.ai>
2021-08-19 15:05:11 +0200

    ci: add tests

--- scripts/get-all-test-paths.sh
+++ scripts/get-all-test-paths.sh
@@ -2,14 +2,8 @@
 
 set -ex
 
-declare -a array1=( "tests/unit/*.py" "tests/integration/*.py" "tests/distributed/*.py" "tests/system/*.py")
-declare -a array2=( $(ls -d tests/{unit,integration,distributed,system}/*/ | grep -v '__pycache__' ))
-dest1=( "${array1[@]}" "${array2[@]}" )
-
-declare -a array1=( "tests/daemon/unit/*.py" )
-declare -a array2=( $(ls -d tests/daemon/{unit,integration}/*/ | grep -v '__pycache__' ))
-dest2=( "${array1[@]}" "${array2[@]}" )
-
-dest=( "${dest1[@]}" "${dest2[@]}" )
+declare -a array1=( "tests/unit/*.py" "tests/integration/*.py")
+declare -a array2=( $(ls -d tests/{unit,integration}/*/ | grep -v '__pycache__' ))
+dest=( "${array1[@]}" "${array2[@]}" )
 
 printf '%s\n' "${dest[@]}" | jq -R . | jq -cs .
