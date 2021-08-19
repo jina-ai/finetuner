0e38cc612ef188c066c477d5e0bb6d3b1286879d
bwanglzu <bo.wang@jina.ai>
2021-08-19 15:28:22 +0200

    feat: yield tuple

--- trainer/pytorch/dataset.py
+++ trainer/pytorch/dataset.py
@@ -13,21 +13,17 @@ class JinaSiameseDataset(IterableDataset):
         inputs: Union[Iterable[Document], DocumentArray, DocumentArrayMemmap],
     ):
         self._inputs = inputs
-        self._pairs = set()
-        for doc in self._inputs:
-            for match in doc.matches:
-                self._pairs.add((doc.id, match.id))
 
     def __len__(self):
         return len(self._pairs)
 
     def _parse_da(self):
-        for (doc_id, match_id) in self._pairs:
-            doc = self._inputs[doc_id]
-            match = doc.matches[match_id]
-            yield doc.blob.astype(np.float32), match.blob.astype(
-                np.float32
-            ), np.float32(match.tags['trainer']['label'])
+        for doc in self._inputs:
+            for match in doc.matches:
+                yield (
+                    doc.blob.astype(np.float32),
+                    match.blob.astype(np.float32),
+                ), np.float32(match.tags['trainer']['label'])
 
     def _stream(self):
         return itertools.cycle(self._parse_da())
