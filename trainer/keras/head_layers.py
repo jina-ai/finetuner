c7e9dc2ae0f32b98937621c570e6fe9763a4c4ba
Han Xiao <han.xiao@jina.ai>
2021-08-24 14:40:12 +0200

    fix(keras): fix nan problem in keras

--- trainer/keras/head_layers.py
+++ trainer/keras/head_layers.py
@@ -47,18 +47,14 @@ class TripletLayer(HeadLayer):
         return tf.nn.relu(dist_pos - dist_neg + self._margin)
 
     def get_output_for_metric(self, anchor, positive, negative):
-        normalize_a = tf.nn.l2_normalize(anchor, axis=-1)
-        normalize_p = tf.nn.l2_normalize(positive, axis=-1)
-        normalize_n = tf.nn.l2_normalize(negative, axis=-1)
-        a_p_cos = tf.reduce_sum(tf.multiply(normalize_a, normalize_p), axis=-1)
-        a_n_cos = tf.reduce_sum(tf.multiply(normalize_a, normalize_n), axis=-1)
-        return a_p_cos, a_n_cos
+        dist_pos = tf.reduce_sum(tf.math.squared_difference(anchor, positive), axis=-1)
+        dist_neg = tf.reduce_sum(tf.math.squared_difference(anchor, negative), axis=-1)
+        return dist_pos, dist_neg
 
     def loss_fn(self, pred_val, target_val):
         return tf.keras.losses.mse(target_val, pred_val)
 
     def metric_fn(self, pred_val, target_val):
-        y_positive, y_negative = pred_val
-        s_p = tf.math.count_nonzero(tf.greater(y_positive, 0))
-        s_n = tf.math.count_nonzero(tf.less(y_negative, 0))
-        return (s_p + s_n) / (len(y_positive) + len(y_negative))
+        dist_pos, dist_neg = pred_val
+        s = tf.math.count_nonzero(tf.less(dist_pos, dist_neg))
+        return s / len(target_val)
