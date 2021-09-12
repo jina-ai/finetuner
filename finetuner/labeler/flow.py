5400cabe621d9b677dc320ba21a32817f301aa12
Han Xiao <han.xiao@jina.ai>
2021-09-12 20:48:30 +0200

    refactor: move base class to tuner

--- finetuner/labeler/flow.py
+++ finetuner/labeler/flow.py
@@ -1,18 +1,40 @@
+import tempfile
+from collections import Iterable
 from typing import Optional, Type
 
-from jina import Flow
+from jina import Flow, DocumentArray, DocumentArrayMemmap
 
 from .executor import FTExecutor, DataIterator
+from ..tuner.base import DocumentArrayLike
 
 
-def start_labeler_flow(
+def start_label_flow(
     embed_executor: Type[FTExecutor],
-    dam_path: str,
-    labeled_dam_path: str,
+    unlabeled_data: DocumentArrayLike,
     clear_labels_on_start: bool = False,
     port_expose: Optional[int] = None,
-    **kwargs
+    **kwargs,
 ):
+    if callable(unlabeled_data):
+        unlabeled_data = unlabeled_data()
+
+    if isinstance(unlabeled_data, DocumentArray):
+        dam_path = tempfile.mkdtemp()
+        dam = DocumentArrayMemmap(dam_path)
+        dam.extend(unlabeled_data)
+    elif isinstance(unlabeled_data, DocumentArrayMemmap):
+        dam_path = unlabeled_data._path
+    elif isinstance(unlabeled_data, str):
+        dam_path = unlabeled_data
+    elif isinstance(unlabeled_data, Iterable):
+        dam_path = tempfile.mkdtemp()
+        dam = DocumentArrayMemmap(dam_path)
+        dam.extend(unlabeled_data)
+    else:
+        raise TypeError(f'{unlabeled_data} is not supported')
+
+    labeled_dam_path = f'{dam_path}/labeled'
+
     f = (
         Flow(protocol='http', port_expose=port_expose, cors=True)
         .add(
