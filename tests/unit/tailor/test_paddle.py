43c2ee583d94e9bc8cb4d6dfc0c36fe1fa990560
bwanglzu <bo.wang@jina.ai>
2021-09-24 14:28:00 +0200

    feat: skip param trainable if stop gradient true

--- tests/unit/tailor/test_paddle.py
+++ tests/unit/tailor/test_paddle.py
@@ -1,8 +1,39 @@
 import pytest
 
 import paddle
+import paddle.nn as nn
 
 from finetuner.tailor.paddle import trim, freeze
+
+
+@pytest.fixture
+def dense_model():
+    return nn.Sequential(
+        nn.Linear(in_features=128, out_features=64),
+        nn.ReLU(),
+        nn.Linear(in_features=64, out_features=32),
+        nn.ReLU(),
+        nn.Linear(in_features=32, out_features=10),
+        nn.Softmax(),
+    )
+
+
+@pytest.fixture
+def simple_cnn_model():
+    return nn.Sequential(
+        nn.Conv2D(1, 4, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm2D(4),
+        nn.ReLU(),
+        nn.MaxPool2D(kernel_size=2, stride=2),
+        nn.Conv2D(4, 4, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm2D(4),
+        nn.ReLU(),
+        nn.MaxPool2D(kernel_size=2, stride=2),
+        nn.Dropout(0.2),
+        nn.Flatten(),
+        nn.Linear(in_features=196, out_features=10),
+        nn.Softmax(),
+    )
 
 
 @pytest.fixture
@@ -10,19 +41,20 @@ def vgg16_cnn_model():
     return paddle.vision.models.vgg16(pretrained=False)
 
 
-@pytest.fixture(params=['vgg16_cnn_model'])
+@pytest.fixture(params=['dense_model', 'simple_cnn_model', 'vgg16_cnn_model'])
 def model(request):
     return request.getfixturevalue(request.param)
 
 
 @pytest.mark.parametrize(
     'model',
-    ['vgg16_cnn_model'],
+    ['dense_model', 'simple_cnn_model', 'vgg16_cnn_model'],
     indirect=['model'],
 )
 def test_freeze(model):
     for param in model.parameters():
-        assert param.trainable
+        if not param.stop_gradient:
+            assert param.trainable
     model = freeze(model)
     for param in model.parameters():
         assert not param.trainable
