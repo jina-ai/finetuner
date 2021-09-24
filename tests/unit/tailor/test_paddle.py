7c797d23d9544bfe7d2d7df38db2bd33073fa050
bwanglzu <bo.wang@jina.ai>
2021-09-24 16:14:48 +0200

    feat: add lstm model

--- tests/unit/tailor/test_paddle.py
+++ tests/unit/tailor/test_paddle.py
@@ -41,14 +41,41 @@ def vgg16_cnn_model():
     return paddle.vision.models.vgg16(pretrained=False)
 
 
-@pytest.fixture(params=['dense_model', 'simple_cnn_model', 'vgg16_cnn_model'])
+@pytest.fixture
+def lstm_model():
+    class Encoder(nn.Layer):
+        def __init__(self, seq_len=128, no_features=128, embedding_size=1024):
+            super().__init__()
+            self.seq_len = seq_len
+            self.no_features = no_features
+            self.embedding_size = embedding_size
+            self.hidden_size = 2 * embedding_size
+            self.lstm = nn.LSTM(
+                input_size=self.no_features,
+                hidden_size=embedding_size,
+                num_layers=1,
+            )
+            self.fc = nn.Linear(1024, 10)
+
+        def forward(self, x):
+            x, (hidden_state, cell_state) = self.lstm(x)
+            last_lstm_layer_hidden_state = hidden_state[-1, :, :]
+            pred = self.fc(last_lstm_layer_hidden_state)
+            return pred
+
+    return Encoder()
+
+
+@pytest.fixture(
+    params=['dense_model', 'simple_cnn_model', 'vgg16_cnn_model', 'lstm_model']
+)
 def model(request):
     return request.getfixturevalue(request.param)
 
 
 @pytest.mark.parametrize(
     'model',
-    ['dense_model', 'simple_cnn_model', 'vgg16_cnn_model'],
+    ['dense_model', 'simple_cnn_model', 'vgg16_cnn_model', 'lstm_model'],
     indirect=['model'],
 )
 def test_freeze(model):
@@ -66,6 +93,7 @@ def test_freeze(model):
         ('dense_model', 4, (128,), (1, 128), [1, 32]),
         ('simple_cnn_model', 11, (1, 28, 28), (1, 1, 28, 28), [1, 10]),
         ('vgg16_cnn_model', 35, (3, 224, 224), (1, 3, 224, 224), [1, 4096]),
+        ('lstm_model', 1, (1, 128), (1, 1, 128), [1, 1024]),
     ],
     indirect=['model'],
 )
