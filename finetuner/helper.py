8d07d25fe2aad99e9ada45a1ee1817ac01e53168
Han Xiao <han.xiao@jina.ai>
2021-10-06 11:40:06 +0200

    docs(helper): add docstring for types

--- finetuner/helper.py
+++ finetuner/helper.py
@@ -2,22 +2,30 @@ from typing import TypeVar, Sequence, Iterator, Union, Callable, List, Dict, Any
 
 from jina import Document, DocumentArray, DocumentArrayMemmap
 
-AnyDNN = TypeVar('AnyDNN')  #: Any implementation of a Deep Neural Network object
-AnyDataLoader = TypeVar('AnyDataLoader')  #: Any implementation of a data loader
+AnyDNN = TypeVar(
+    'AnyDNN'
+)  #: The type of any implementation of a Deep Neural Network object
+AnyDataLoader = TypeVar(
+    'AnyDataLoader'
+)  #: The type of any implementation of a data loader
 DocumentSequence = TypeVar(
     'DocumentSequence',
     Sequence[Document],
     DocumentArray,
     DocumentArrayMemmap,
     Iterator[Document],
-)
+)  #: The type of any sequence of Document
 DocumentArrayLike = Union[
     DocumentSequence,
     Callable[..., DocumentSequence],
-]
+]  #: The type :py:data:`DocumentSequence` or a function that gives :py:data:`DocumentSequence`
 
-EmbeddingLayerInfoType = List[Dict[str, Any]]
-TunerReturnType = Dict[str, Dict[str, Any]]
+EmbeddingLayerInfoType = List[
+    Dict[str, Any]
+]  #: The type of embedding layer information used in Tailor
+TunerReturnType = Dict[
+    str, Dict[str, Any]
+]  #: The type of loss, metric information Tuner returns
 
 
 def get_framework(dnn_model: AnyDNN) -> str:
