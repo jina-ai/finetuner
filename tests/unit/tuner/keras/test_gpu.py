fb605fbdc31cde3a3528191b54e472f3c57762d8
bwanglzu <bo.wang@jina.ai>
2021-10-13 19:52:47 +0200

    feat(tuner): comment out keras gpu

--- tests/unit/tuner/keras/test_gpu.py
+++ tests/unit/tuner/keras/test_gpu.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 from finetuner.tuner.keras import KerasTuner
 
 
-@pytest.mark.gpu
-@pytest.mark.parametrize('head_layer', ['TripletLayer'])
+# @pytest.mark.gpu
+@pytest.mark.parametrize('head_layer', ['TripletLayer', 'CosineLayer'])
 def test_gpu_keras(generate_random_triplets, head_layer, caplog):
     tf.debugging.set_log_device_placement(True)
     data = generate_random_triplets(4, 4)
