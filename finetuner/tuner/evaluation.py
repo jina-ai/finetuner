b2ee822004597fe17f0617506d7563651c4e8cf3
Maximilian Werk <maximilian.werk@jina.ai>
2021-10-14 11:06:25 +0200

    feat: fix ndcg

--- finetuner/tuner/evaluation.py
+++ finetuner/tuner/evaluation.py
@@ -51,56 +51,16 @@ def get_ndcg_at_n(to_be_scored_docs, n=-1):
     for doc in to_be_scored_docs:
         dcg = 0
         positive_ids = doc.tags['positive_ids']
-        pos_counter = 0
-        for position, match in enumerate(doc.matches[:n]):
+        first_n = doc.matches[:n]
+        for position, match in enumerate(first_n):
             if match.id in positive_ids:
                 dcg += 1 / np.log(position + 2)
-                pos_counter += 1
-        idcg = max(_get_idcg(pos_counter), 1e-10)
+
+        max_positives = min(len(positive_ids), len(first_n))
+        idcg = max(_get_idcg(max_positives), 1e-10)
         ndcg += dcg / idcg
     return ndcg
 
 
 def _get_idcg(n):
     return sum(1 / np.log(position + 2) for position in range(n))
-
-
-def get_map(data):
-    pass
-
-
-'''
-
-usual data structure:
-- doc
-    - match (labeled data)
-    - match (labeled data)
-
-
-- compute distances and order results in:
-
-NDCG @ X data structure:
-- doc
-    - match (actual matches according to model distance)
-    - match (actual matches according to model distance)
-     ....
-
-
-sum(doc of docs) ...
-
-100k items in shop
-100
-100k * 100
-
-10 million clicks:
-
-9M => train data
-1M => eval data
-100k Documents with 1M labels
-NDCG only for 1k Documents: remove labels(matches) from 99k Documents
-
-Doc1
-    - 50 positive
-    - 50 negative
-    - 1000 unknown
-'''
