5529d7769e28152c5cfef7f31a78844ddb0f8cb9
Han Xiao <han.xiao@jina.ai>
2021-10-15 10:14:40 +0200

    refactor(helper): move get_tailor and get_tunner to helper

--- finetuner/tuner/__init__.py
+++ finetuner/tuner/__init__.py
@@ -1,25 +1,6 @@
-from typing import Optional, Dict
+from typing import Optional
 
-from ..helper import AnyDNN, DocumentArrayLike, get_framework, TunerReturnType
-
-
-def _get_tuner_class(embed_model):
-    f_type = get_framework(embed_model)
-
-    if f_type == 'keras':
-        from .keras import KerasTuner
-
-        return KerasTuner
-    elif f_type == 'torch':
-        from .pytorch import PytorchTuner
-
-        return PytorchTuner
-    elif f_type == 'paddle':
-        from .paddle import PaddleTuner
-
-        return PaddleTuner
-    else:
-        raise ValueError('Could not identify backend framework of embed_model.')
+from ..helper import AnyDNN, DocumentArrayLike, TunerReturnType, get_tuner_class
 
 
 def fit(
@@ -32,14 +13,14 @@ def fit(
     device: str = 'cpu',
     **kwargs
 ) -> TunerReturnType:
-    ft = _get_tuner_class(embed_model)
+    ft = get_tuner_class(embed_model)
 
     return ft(embed_model, head_layer=head_layer).fit(
         train_data, eval_data, epochs=epochs, batch_size=batch_size, device=device
     )
 
 
-def save(embed_model, model_path):
-    ft = _get_tuner_class(embed_model)
+def save(embed_model: AnyDNN, model_path: str, *args, **kwargs) -> None:
+    ft = get_tuner_class(embed_model)
 
-    ft(embed_model).save(model_path)
+    ft(embed_model).save(model_path * args, **kwargs)
