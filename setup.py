d9d04803bc5d484c9bb98db671e3569b1212651d
Han Xiao <han.xiao@jina.ai>
2021-10-24 19:04:49 +0200

    feat(tuner): add plot function for tuner.summary

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     long_description_content_type='text/markdown',
     zip_safe=False,
     setup_requires=['setuptools>=18.0', 'wheel'],
-    install_requires=['jina>=2.1.11'],
+    install_requires=['jina>=2.1.11', 'matplotlib'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
