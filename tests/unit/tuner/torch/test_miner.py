bed777fa8171bdcf32708eab6792aa53393ebab7
bwanglzu <bo.wang@jina.ai>
2021-11-02 17:12:08 +0100

    feat(tuner): add siamese session miner test

--- tests/unit/tuner/torch/test_miner.py
+++ tests/unit/tuner/torch/test_miner.py
@@ -1,7 +1,12 @@
 import pytest
 import torch
 
-from finetuner.tuner.pytorch.miner import SiameseMiner, TripletMiner
+from finetuner.tuner.pytorch.miner import (
+    SiameseMiner,
+    TripletMiner,
+    SiameseSessionMiner,
+    TripletSessionMiner,
+)
 
 BATCH_SIZE = 8
 NUM_DIM = 10
@@ -18,6 +23,16 @@ def triplet_miner():
 
 
 @pytest.fixture
+def siamese_session_miner():
+    return SiameseSessionMiner()
+
+
+@pytest.fixture
+def triplet_session_miner():
+    return TripletSessionMiner()
+
+
+@pytest.fixture
 def embeddings():
     return [torch.rand(NUM_DIM) for _ in range(BATCH_SIZE)]
 
@@ -25,6 +40,11 @@ def embeddings():
 @pytest.fixture
 def labels():
     return [1, 3, 1, 3, 2, 4, 2, 4]
+
+
+@pytest.fixture
+def session_labels():
+    return [(1, 1), (2, -1), (2, 1), (1, -1), (3, -1), (3, 1), (3, -1), (3, -1)]
 
 
 def test_siamese_miner(embeddings, labels, siamese_miner):
@@ -74,4 +94,31 @@ def test_triplet_miner_given_insufficient_inputs(
     embeddings = embeddings[:cut_index]
     labels = labels[:cut_index]
     rv = list(siamese_miner.mine(embeddings, labels))
+    assert len(rv) == 0
+
+
+def test_siamese_session_miner(embeddings, session_labels, siamese_session_miner):
+    rv = siamese_session_miner.mine(embeddings, session_labels)
+    assert (
+        len(rv) == 8
+    )  # 1 pair with session 1, 1 pair with session 2, 6 pairs with session 3
+    for item in rv:
+        idx_left, idx_right, label = item
+        # find corresponded label idx
+        label_left = session_labels[idx_left]
+        label_right = session_labels[idx_right]
+        if label_left == label_right:
+            expected_label = 1
+        else:
+            expected_label = -1
+        assert label == expected_label
+
+
+@pytest.mark.parametrize('cut_index', [0, 1])
+def test_siamese_session_miner_given_insufficient_inputs(
+    embeddings, session_labels, siamese_session_miner, cut_index
+):
+    embeddings = embeddings[:cut_index]
+    session_labels = session_labels[:cut_index]
+    rv = list(siamese_session_miner.mine(embeddings, session_labels))
     assert len(rv) == 0
