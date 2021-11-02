901a48eb1a9cadc5b690eac8db3977a7520bdb59
bwanglzu <bo.wang@jina.ai>
2021-11-02 15:08:28 +0100

    feat(tuner): add session dataset

--- finetuner/tuner/pytorch/miner.py
+++ finetuner/tuner/pytorch/miner.py
@@ -1,9 +1,27 @@
 import numpy as np
 from typing import List, Tuple
-from itertools import combinations
+from itertools import combinations, groupby
 
 from ..base import BaseMiner
 from ...helper import AnyTensor
+
+
+def _generate_all_possible_pairs(labels: List[int]):
+    return [
+        (left[0], right[0], 1) if left[1] == right[1] else (left[0], right[0], -1)
+        for left, right in combinations(enumerate(labels), 2)
+    ]
+
+
+def _generate_all_possible_triplets(labels: List[int]):
+    labels1 = np.expand_dims(labels, 1)
+    labels2 = np.expand_dims(labels, 0)
+    matches = (labels1 == labels2).astype(int)
+    diffs = matches ^ 1
+    np.fill_diagonal(matches, 0)
+    triplets = np.expand_dims(matches, 2) * np.expand_dims(diffs, 1)
+    idxes_anchor, idxes_pos, idxes_neg = np.where(triplets)
+    return list(zip(idxes_anchor, idxes_pos, idxes_neg))
 
 
 class SiameseMiner(BaseMiner):
@@ -17,10 +35,7 @@ class SiameseMiner(BaseMiner):
         :return: a pair of label indices and their label as tuple.
         """
         assert len(embeddings) == len(labels)
-        return [
-            (left[0], right[0], 1) if left[1] == right[1] else (left[0], right[0], -1)
-            for left, right in combinations(enumerate(labels), 2)
-        ]
+        return _generate_all_possible_pairs(labels)
 
 
 class TripletMiner(BaseMiner):
@@ -34,14 +49,7 @@ class TripletMiner(BaseMiner):
         :return: triplet of label indices follows the order of anchor, positive and negative.
         """
         assert len(embeddings) == len(labels)
-        labels1 = np.expand_dims(labels, 1)
-        labels2 = np.expand_dims(labels, 0)
-        matches = (labels1 == labels2).astype(int)
-        diffs = matches ^ 1
-        np.fill_diagonal(matches, 0)
-        triplets = np.expand_dims(matches, 2) * np.expand_dims(diffs, 1)
-        idxes_anchor, idxes_pos, idxes_neg = np.where(triplets)
-        return list(zip(idxes_anchor, idxes_pos, idxes_neg))
+        return _generate_all_possible_triplets(labels)
 
 
 class SiameseSessionMiner(BaseMiner):
@@ -52,9 +60,19 @@ class SiameseSessionMiner(BaseMiner):
 
         :param embeddings: embeddings from model, should be a list of Tensor objects.
         :param labels: labels of each embeddings, consist of session id and label.
+          the labels are either -1 or 1.
         :return: a pair of label indices and their label as tuple.
         """
+        rv = []
         assert len(embeddings) == len(labels)
+        for session, group in groupby(
+            sorted(labels, key=lambda x: x[0]), lambda x: x[0]
+        ):
+            session_labels = [item for _, item in group]
+            rv.extend(
+                _generate_all_possible_pairs(session_labels)
+            )  # all possible tuples in the session
+        return rv
 
 
 class TripletSessionMiner(BaseMiner):
@@ -67,4 +85,11 @@ class TripletSessionMiner(BaseMiner):
         :param labels: labels of each embeddings, consist of session id and label.
         :return: triplet of label indices follows the order of anchor, positive and negative.
         """
-        pass
+        rv = []
+        assert len(embeddings) == len(labels)
+        for session, group in groupby(
+            sorted(labels, key=lambda x: x[0]), lambda x: x[0]
+        ):
+            session_labels = [item for _, item in group]
+            rv.extend(_generate_all_possible_triplets(session_labels))
+        return rv
