7aaaac99c119e827fc823c8b659d9add42a75d8a
bwanglzu <bo.wang@jina.ai>
2021-11-03 18:20:22 +0100

    feat(tuner): try to fix tf session

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,9 @@ import tensorflow as tf
 @pytest.fixture(autouse=True)
 def clear_session():
     tf.keras.backend.clear_session()
+    config_proto = tf.ConfigProto()
+    config_proto.graph_options.rewrite_options.arithmetic_optimization = (
+        tf.core.protobuf.rewriter_config_pb2.RewriterConfig.OFF
+    )
+    session = tf.Session(config=config_proto)
+    tf.python.keras.backend.set_session(session)
