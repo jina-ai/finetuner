d80a4d0faba7b504dcc93fe9769242f331df1484
Han Xiao <han.xiao@jina.ai>
2021-11-05 12:34:29 +0100

    test(embedding): add test for #190

--- tests/unit/test_embedding.py
+++ tests/unit/test_embedding.py
@@ -2,7 +2,8 @@ import paddle
 import pytest
 import tensorflow as tf
 import torch
-from jina import DocumentArray, DocumentArrayMemmap
+from jina import DocumentArray, DocumentArrayMemmap, Document
+import numpy as np
 
 from finetuner.embedding import embed
 from finetuner.toydata import generate_fashion_match
@@ -34,6 +35,43 @@ embed_models = {
         paddle.nn.Linear(in_features=128, out_features=32),
     ),
 }
+
+random_embed_models = {
+    'keras': lambda: tf.keras.Sequential(
+        [tf.keras.layers.Dropout(0.5), tf.keras.layers.BatchNormalization()]
+    ),
+    'pytorch': lambda: torch.nn.Sequential(
+        torch.nn.Dropout(0.5), torch.nn.BatchNorm1d(128)
+    ),
+    'paddle': lambda: paddle.nn.Sequential(
+        paddle.nn.Dropout(0.5), paddle.nn.BatchNorm1D(128)
+    ),
+}
+
+
+@pytest.mark.parametrize('framework', ['keras', 'pytorch', 'paddle'])
+def test_embedding_on_random_network(framework):
+    docs = DocumentArray([Document() for _ in range(2)])
+    docs.blobs = np.random.random([2, 128]).astype(np.float32)
+    embed_model = random_embed_models[framework]()
+    embed(docs, embed_model)
+
+    embed1 = docs.embeddings.copy()
+
+    # reset
+    docs.embeddings = np.random.random([2, 128]).astype(np.float32)
+
+    # try it again, it should yield the same result
+    embed(docs, embed_model)
+    np.testing.assert_array_almost_equal(docs.embeddings, embed1)
+
+    # reset
+    docs.embeddings = np.random.random([2, 128]).astype(np.float32)
+
+    # now do this one by one
+    embed(docs[:1], embed_model)
+    embed(docs[-1:], embed_model)
+    np.testing.assert_array_almost_equal(docs.embeddings, embed1)
 
 
 @pytest.mark.parametrize('framework', ['keras', 'pytorch', 'paddle'])
