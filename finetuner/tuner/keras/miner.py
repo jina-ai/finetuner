00053ac84139a43d37b12a8e13ec6cce0d402ccd
Wang Bo <bo.wang@jina.ai>
2021-11-15 21:05:40 +0100

    feat(miner): refactor tf pipeline (#198)
    * feat(miner): refactor rf miner
    
    * feat: add simese miner
    
    * feat(tuner): add tf triplet miner
    
    * feat(tuner): add tf session miner
    
    * feat(tuner): add tf session miner
    
    * feat(tuner): add gpu test miner
    
    * feat(tuner): add get distance
    
    * feat(tuner): add loss functions
    
    * feat(tuner): add loss functions
    
    * feat(tuner): clear up prints

---
+++ finetuner/tuner/keras/miner.py
@@ -0,0 +1,153 @@
+from itertools import combinations, groupby, permutations
+from typing import Tuple
+
+import tensorflow as tf
+
+from ..base import BaseClassMiner, BaseSessionMiner
+
+
+class SiameseMiner(BaseClassMiner[tf.Tensor]):
+    def mine(
+        self, labels: tf.Tensor, distances: tf.Tensor
+    ) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+        """Generate all possible pairs.
+
+        :param labels: A 1D tensor of item labels (classes)
+        :param distances: A tensor matrix of pairwise distance between each two item
+            embeddings
+
+        :return: three 1D tensors, first one holding integers of first element of
+            pair, second of the second element of pair, and third one the label (0 or
+            1) for the pair for each pair
+        """
+        assert len(distances) == len(labels)
+
+        l1, l2 = tf.expand_dims(labels, 1), tf.expand_dims(labels, 0)
+        matches = tf.cast(l1 == l2, tf.uint8)
+        diffs = 1 - matches
+        matches = tf.experimental.numpy.triu(matches, 1)
+        diffs = tf.experimental.numpy.triu(diffs)
+
+        ind1_pos, ind2_pos = tf.unstack(tf.where(matches), axis=1)
+        ind1_neg, ind2_neg = tf.unstack(tf.where(diffs), axis=1)
+
+        ind1 = tf.concat([ind1_pos, ind1_neg], axis=0)
+        ind2 = tf.concat([ind2_pos, ind2_neg], axis=0)
+
+        target = tf.concat([tf.ones_like(ind1_pos), tf.zeros_like(ind1_neg)], axis=0)
+        return ind1, ind2, target
+
+
+class TripletMiner(BaseClassMiner[tf.Tensor]):
+    def mine(
+        self, labels: tf.Tensor, distances: tf.Tensor
+    ) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+        """Generate all possible triplets.
+
+        :param labels: A 1D tensor of item labels (classes)
+        :param distances: A tensor matrix of pairwise distance between each two item
+            embeddings
+
+        :return: three 1D tensors, holding the anchor index, positive index and
+            negative index of each triplet, respectively
+        """
+        assert len(distances) == len(labels)
+
+        l1, l2 = tf.expand_dims(labels, 1), tf.expand_dims(labels, 0)
+        matches = tf.cast(l1 == l2, tf.uint8)
+        diffs = matches ^ 1
+
+        matches = tf.linalg.set_diag(matches, diagonal=tf.zeros_like(labels, tf.uint8))
+        triplets = tf.expand_dims(matches, 2) * tf.expand_dims(diffs, 1)
+
+        return tf.transpose(tf.where(triplets))
+
+
+class SiameseSessionMiner(BaseSessionMiner[tf.Tensor]):
+    def mine(
+        self, labels: Tuple[tf.Tensor, tf.Tensor], distances: tf.Tensor
+    ) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+        """Generate all possible pairs for each session.
+
+        :param labels: A tuple of 1D tensors, denotind the items' session and match
+            type (0 for anchor, 1 for postive match and -1 for negative match),
+            respectively
+        :param distances: A tensor matrix of pairwise distance between each two item
+            embeddings
+
+        :return: three numpy arrays, first one holding integers of first element of
+            pair, second of the second element of pair, and third one the label (0 or
+            1) for the pair for each pair
+        """
+        assert len(distances) == len(labels[0]) == len(labels[1])
+
+        sessions, match_types = [x.numpy().tolist() for x in labels]
+        ind_one, ind_two, labels_ret = [], [], []
+
+        labels_index = [
+            (sess, match_type, index)
+            for index, (sess, match_type) in enumerate(zip(sessions, match_types))
+        ]
+        sorted_labels_with_index = sorted(labels_index, key=lambda x: x[0])
+
+        for _, group in groupby(sorted_labels_with_index, lambda x: x[0]):
+            for left, right in combinations(group, 2):  # (session_id, label, ind)
+                if left[1] != -1 or right[1] != -1:
+                    ind_one.append(left[2])
+                    ind_two.append(right[2])
+                    labels_ret.append(0 if min(left[1], right[1]) == -1 else 1)
+
+        return (
+            tf.constant(ind_one),
+            tf.constant(ind_two),
+            tf.constant(labels_ret),
+        )
+
+
+class TripletSessionMiner(BaseSessionMiner[tf.Tensor]):
+    def mine(
+        self, labels: Tuple[tf.Tensor, tf.Tensor], distances: tf.Tensor
+    ) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+        """Generate all possible triplets for each session.
+
+        :param labels: A tuple of 1D tensors, denotind the items' session and match
+            type (0 for anchor, 1 for postive match and -1 for negative match),
+            respectively
+        :param distances: A tensor matrix of pairwise distance between each two item
+            embeddings
+
+        :return: three numpy arrays, holding the anchor index, positive index and
+            negative index of each triplet, respectively
+        """
+
+        assert len(distances) == len(labels[0]) == len(labels[1])
+
+        sessions, match_types = [x.numpy().tolist() for x in labels]
+        anchor_ind, pos_ind, neg_ind = [], [], []
+
+        labels_index = [
+            (sess, match_type, index)
+            for index, (sess, match_type) in enumerate(zip(sessions, match_types))
+        ]
+        sorted_labels_with_index = sorted(labels_index, key=lambda x: x[0])
+
+        for _, group in groupby(sorted_labels_with_index, lambda x: x[0]):
+            anchor_pos_session_indices = []
+            neg_session_indices = []
+
+            for _, session_label, session_index in group:
+                if session_label >= 0:
+                    anchor_pos_session_indices.append(session_index)
+                else:
+                    neg_session_indices.append(session_index)
+
+            for anchor, pos in permutations(anchor_pos_session_indices, 2):
+                anchor_ind += [anchor] * len(neg_session_indices)
+                pos_ind += [pos] * len(neg_session_indices)
+                neg_ind += neg_session_indices
+
+        return (
+            tf.constant(anchor_ind),
+            tf.constant(pos_ind),
+            tf.constant(neg_ind),
+        )
---
+++ tests/unit/tuner/keras/test_miner.py
@@ -0,0 +1,212 @@
+import numpy as np
+import pytest
+import tensorflow as tf
+
+from finetuner.tuner.keras.miner import (
+    SiameseMiner,
+    TripletMiner,
+    SiameseSessionMiner,
+    TripletSessionMiner,
+)
+
+
+def fake_dists(size):
+    return 1 - tf.eye(size)
+
+
+@pytest.fixture
+def labels():
+    return tf.constant([1, 3, 1, 3, 2, 2])
+
+
+@pytest.fixture
+def session_labels():
+    return (
+        tf.constant([1, 2, 2, 2, 1, 1, 2, 2]),
+        tf.constant([1, 1, 0, -1, 0, -1, -1, 1]),
+    )
+
+
+def test_siamese_miner(labels):
+    tuples = np.array(
+        [
+            (0, 2, 1),
+            (1, 3, 1),
+            (4, 5, 1),
+            (0, 1, 0),
+            (0, 3, 0),
+            (0, 4, 0),
+            (0, 5, 0),
+            (1, 2, 0),
+            (1, 4, 0),
+            (1, 5, 0),
+            (2, 3, 0),
+            (2, 4, 0),
+            (2, 5, 0),
+            (3, 4, 0),
+            (3, 5, 0),
+        ]
+    )
+    true_ind_one, true_ind_two, true_label = tuples.T
+    ind_one, ind_two, label = SiameseMiner().mine(labels, fake_dists(len(labels)))
+
+    np.testing.assert_equal(true_ind_one, ind_one.numpy())
+    np.testing.assert_equal(true_ind_two, ind_two.numpy())
+    np.testing.assert_equal(true_label, label.numpy())
+
+
+@pytest.mark.parametrize('cut_index', [0, 1])
+def test_siamese_miner_given_insufficient_inputs(labels, cut_index):
+    labels = labels[:cut_index]
+    ind_one, ind_two, label = SiameseMiner().mine(labels, fake_dists(len(labels)))
+    assert len(ind_one) == 0
+    assert len(ind_two) == 0
+    assert len(label) == 0
+
+
+@pytest.mark.parametrize('cut_index', [0, 1])
+def test_siamese_miner_given_insufficient_inputs(labels, cut_index):
+    labels = labels[:cut_index]
+    ind_one, ind_two, label = SiameseMiner().mine(labels, fake_dists(len(labels)))
+    assert len(ind_one) == 0
+    assert len(ind_two) == 0
+    assert len(label) == 0
+
+
+def test_triplet_miner(labels):
+    triplets = np.array(
+        [
+            (0, 2, 1),
+            (0, 2, 3),
+            (0, 2, 4),
+            (0, 2, 5),
+            (1, 3, 0),
+            (1, 3, 2),
+            (1, 3, 4),
+            (1, 3, 5),
+            (2, 0, 1),
+            (2, 0, 3),
+            (2, 0, 4),
+            (2, 0, 5),
+            (3, 1, 0),
+            (3, 1, 2),
+            (3, 1, 4),
+            (3, 1, 5),
+            (4, 5, 0),
+            (4, 5, 1),
+            (4, 5, 2),
+            (4, 5, 3),
+            (5, 4, 0),
+            (5, 4, 1),
+            (5, 4, 2),
+            (5, 4, 3),
+        ]
+    )
+    true_anch_ind, true_pos_ind, true_neg_ind = triplets.T
+    anch_ind, pos_ind, neg_ind = TripletMiner().mine(labels, fake_dists(len(labels)))
+
+    np.testing.assert_equal(anch_ind.numpy(), true_anch_ind)
+    np.testing.assert_equal(pos_ind.numpy(), true_pos_ind)
+    np.testing.assert_equal(neg_ind.numpy(), true_neg_ind)
+
+
+@pytest.mark.parametrize('cut_index', [0, 1])
+def test_triplet_miner_given_insufficient_inputs(labels, cut_index):
+    labels = labels[:cut_index]
+    anch_ind, pos_ind, neg_ind = TripletMiner().mine(labels, fake_dists(len(labels)))
+    assert len(anch_ind) == 0
+    assert len(pos_ind) == 0
+    assert len(neg_ind) == 0
+
+
+def test_siamese_session_miner(session_labels):
+    tuples = np.array(
+        [
+            (0, 4, 1),
+            (0, 5, 0),
+            (4, 5, 0),
+            (1, 2, 1),
+            (1, 3, 0),
+            (1, 6, 0),
+            (1, 7, 1),
+            (2, 3, 0),
+            (2, 6, 0),
+            (2, 7, 1),
+            (3, 7, 0),
+            (6, 7, 0),
+        ]
+    )
+    true_ind_one, true_ind_two, true_label = tuples.T
+    ind_one, ind_two, label = SiameseSessionMiner().mine(
+        session_labels, fake_dists(len(session_labels[0]))
+    )
+    np.testing.assert_equal(true_ind_one, ind_one.numpy())
+    np.testing.assert_equal(true_ind_two, ind_two.numpy())
+    np.testing.assert_equal(true_label, label.numpy())
+
+
+@pytest.mark.parametrize('cut_index', [0, 1])
+def test_siamese_session_miner_given_insufficient_inputs(session_labels, cut_index):
+    session_labels = [x[:cut_index] for x in session_labels]
+    ind_one, ind_two, label = SiameseSessionMiner().mine(
+        session_labels, fake_dists(len(session_labels[0]))
+    )
+    assert len(ind_one) == 0
+    assert len(ind_two) == 0
+    assert len(label) == 0
+
+
+def test_triplet_session_miner(session_labels):
+    triplets = np.array(
+        [
+            (0, 4, 5),
+            (4, 0, 5),
+            (1, 2, 3),
+            (1, 2, 6),
+            (1, 7, 3),
+            (1, 7, 6),
+            (2, 1, 3),
+            (2, 1, 6),
+            (2, 7, 3),
+            (2, 7, 6),
+            (7, 1, 3),
+            (7, 1, 6),
+            (7, 2, 3),
+            (7, 2, 6),
+        ]
+    )
+    true_anch_ind, true_pos_ind, true_neg_ind = triplets.T
+    anch_ind, pos_ind, neg_ind = TripletSessionMiner().mine(
+        session_labels, fake_dists(len(session_labels[0]))
+    )
+
+    np.testing.assert_equal(anch_ind.numpy(), true_anch_ind)
+    np.testing.assert_equal(pos_ind.numpy(), true_pos_ind)
+    np.testing.assert_equal(neg_ind.numpy(), true_neg_ind)
+
+
+@pytest.mark.parametrize('cut_index', [0, 1])
+def test_triplet_session_miner_given_insufficient_inputs(session_labels, cut_index):
+    session_labels = [x[:cut_index] for x in session_labels]
+    anch_ind, pos_ind, neg_ind = TripletSessionMiner().mine(
+        session_labels, fake_dists(len(session_labels[0]))
+    )
+    assert len(anch_ind) == 0
+    assert len(pos_ind) == 0
+    assert len(neg_ind) == 0
+
+
+@pytest.mark.gpu
+@pytest.mark.parametrize('miner', [SiameseMiner, TripletMiner])
+def test_class_miner_gpu(miner, labels, tf_gpu_config):
+    with tf.device('/GPU:0'):
+        m = miner()
+        _ = m.mine(labels, fake_dists(len(labels)))
+
+
+@pytest.mark.gpu
+@pytest.mark.parametrize('miner', [SiameseSessionMiner, TripletSessionMiner])
+def test_session_miner_gpu(miner, session_labels, tf_gpu_config):
+    with tf.device('/GPU:0'):
+        m = miner()
+        _ = m.mine(labels, fake_dists(len(labels[0])))
