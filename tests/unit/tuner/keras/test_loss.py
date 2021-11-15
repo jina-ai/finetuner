462e83f6993e232f96687b9e1d07bccb8a22b654
bwanglzu <bo.wang@jina.ai>
2021-11-15 16:51:53 +0100

    feat(tuner): add loss functions

--- tests/unit/tuner/keras/test_loss.py
+++ tests/unit/tuner/keras/test_loss.py
@@ -10,25 +10,25 @@ N_DIM = 128
 ALL_LOSSES = [SiameseLoss, TripletLoss]
 
 
-# @pytest.mark.parametrize('margin', [0.0, 0.5, 1.0])
-# @pytest.mark.parametrize('distance', ['cosine', 'euclidean'])
-# @pytest.mark.parametrize('loss_cls', ALL_LOSSES)
-# def test_loss_output(loss_cls, distance, margin):
-#     """Test that we get a single positive number as output"""
-#     loss = loss_cls(distance=distance, margin=margin)
-#
-#     labels = np.ones((N_BATCH,))
-#     labels[: N_BATCH // 2] = 0
-#     labels = tf.convert_to_tensor(labels)
-#     embeddings = tf.random.uniform((N_BATCH, N_DIM))
-#
-#     output = loss(embeddings, labels)
-#
-#     print(output)
-#     print(type(output))
-#
-#     # assert output.ndim == 0
-#     assert output >= 0
+@pytest.mark.parametrize('margin', [0.0, 0.5, 1.0])
+@pytest.mark.parametrize('distance', ['cosine', 'euclidean'])
+@pytest.mark.parametrize('loss_cls', ALL_LOSSES)
+def test_loss_output(loss_cls, distance, margin):
+    """Test that we get a single positive number as output"""
+    loss = loss_cls(distance=distance, margin=margin)
+
+    labels = np.ones((N_BATCH,))
+    labels[: N_BATCH // 2] = 0
+    labels = tf.convert_to_tensor(labels)
+    embeddings = tf.random.uniform((N_BATCH, N_DIM))
+
+    output = loss(embeddings, labels)
+
+    print(output)
+    print(type(output))
+
+    # assert output.ndim == 0
+    assert output >= 0
 
 
 @pytest.mark.parametrize('distance', ['cosine', 'euclidean'])
