570f31b3dacf1c230c549714e16bb695238972e9
bwanglzu <bo.wang@jina.ai>
2021-11-16 17:27:47 +0100

    test(keras): add keras gpu test

--- tests/unit/tuner/keras/test_gpu.py
+++ tests/unit/tuner/keras/test_gpu.py
@@ -1,57 +1,48 @@
-# import pytest
-# import tensorflow as tf
-# from jina import DocumentArray, DocumentArrayMemmap
+import pytest
+import tensorflow as tf
+from jina import DocumentArray, DocumentArrayMemmap
 
-# from finetuner.tuner.keras import KerasTuner
-# from finetuner.embedding import embed
-# from finetuner.toydata import generate_fashion
+from finetuner.tuner.keras import KerasTuner
+from finetuner.embedding import embed
+from finetuner.toydata import generate_fashion
 
-# all_test_losses = [
-#     'CosineSiameseLoss',
-#     'CosineTripletLoss',
-#     'EuclideanSiameseLoss',
-#     'EuclideanTripletLoss',
-# ]
-
-
-# @pytest.fixture(autouse=True)
-# def tf_gpu_config():
-#     tf.debugging.set_log_device_placement(True)
-#     gpus = tf.config.list_physical_devices('GPU')
-#     tf.config.set_logical_device_configuration(
-#         gpus[0], [tf.config.LogicalDeviceConfiguration(memory_limit=256)]
-#     )
+all_test_losses = [
+    'CosineSiameseLoss',
+    'CosineTripletLoss',
+    'EuclideanSiameseLoss',
+    'EuclideanTripletLoss',
+]
 
 
-# @pytest.mark.gpu
-# @pytest.mark.parametrize('loss', all_test_losses)
-# def test_gpu_keras(generate_random_triplets, loss):
-#     data = generate_random_triplets(4, 4)
-#     embed_model = tf.keras.models.Sequential()
-#     embed_model.add(tf.keras.layers.InputLayer(input_shape=(4,)))
-#     embed_model.add(tf.keras.layers.Dense(4))
+@pytest.mark.gpu
+@pytest.mark.parametrize('loss', all_test_losses)
+def test_gpu_keras(generate_random_triplets, loss, tf_gpu_config):
+    data = generate_random_triplets(4, 4)
+    embed_model = tf.keras.models.Sequential()
+    embed_model.add(tf.keras.layers.InputLayer(input_shape=(4,)))
+    embed_model.add(tf.keras.layers.Dense(4))
 
-#     tuner = KerasTuner(embed_model, loss)
+    tuner = KerasTuner(embed_model, loss)
 
-#     tuner.fit(data, data, epochs=2, batch_size=4, device='cuda')
+    tuner.fit(data, data, epochs=2, batch_size=4, device='cuda')
 
 
-# @pytest.mark.gpu
-# def test_set_embeddings_gpu(tmpdir):
-#     # works for DA
-#     embed_model = tf.keras.Sequential(
-#         [
-#             tf.keras.layers.Flatten(input_shape=(28, 28)),
-#             tf.keras.layers.Dense(128, activation='relu'),
-#             tf.keras.layers.Dense(32),
-#         ]
-#     )
-#     docs = DocumentArray(generate_fashion(num_total=100))
-#     embed(docs, embed_model, 'cuda')
-#     assert docs.embeddings.shape == (100, 32)
+@pytest.mark.gpu
+def test_set_embeddings_gpu(tmpdir, tf_gpu_config):
+    # works for DA
+    embed_model = tf.keras.Sequential(
+        [
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(32),
+        ]
+    )
+    docs = DocumentArray(generate_fashion(num_total=100))
+    embed(docs, embed_model, 'cuda')
+    assert docs.embeddings.shape == (100, 32)
 
-#     # works for DAM
-#     dam = DocumentArrayMemmap(tmpdir)
-#     dam.extend(generate_fashion(num_total=42))
-#     embed(dam, embed_model, 'cuda')
-#     assert dam.embeddings.shape == (42, 32)
+    # works for DAM
+    dam = DocumentArrayMemmap(tmpdir)
+    dam.extend(generate_fashion(num_total=42))
+    embed(dam, embed_model, 'cuda')
+    assert dam.embeddings.shape == (42, 32)
