59557dcdd49acd57ca5be4d5d15fc6c9c423bee7
Tadej Svetina <tadej.svetina@jina.ai>
2021-11-16 09:26:41 +0100

    fix: try fixing GPU tests

--- tests/unit/tuner/keras/conftest.py
+++ tests/unit/tuner/keras/conftest.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 
 @pytest.fixture
 def tf_gpu_config():
+    tf.keras.backend.clear_session()
     return
     tf.debugging.set_log_device_placement(True)
     gpus = tf.config.list_physical_devices('GPU')
