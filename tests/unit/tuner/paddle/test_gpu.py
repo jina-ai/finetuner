d7d1c1efdb3631d4dbc8b4a7aca98b1589beb819
bwanglzu <bo.wang@jina.ai>
2021-11-17 11:37:29 +0100

    test: increase random data number paddle

--- tests/unit/tuner/paddle/test_gpu.py
+++ tests/unit/tuner/paddle/test_gpu.py
@@ -13,7 +13,7 @@ all_test_losses = ['SiameseLoss', 'TripletLoss']
 @pytest.mark.parametrize('loss', all_test_losses)
 def test_gpu_paddle(generate_random_data, loss):
 
-    data = generate_random_data(4, 4)
+    data = generate_random_data(40, 4)
 
     embed_model = nn.Sequential(
         nn.Linear(in_features=4, out_features=4),
@@ -21,7 +21,7 @@ def test_gpu_paddle(generate_random_data, loss):
 
     tuner = PaddleTuner(embed_model, loss=loss)
 
-    tuner.fit(data, data, epochs=2, batch_size=4, device='cuda')
+    tuner.fit(data, data, epochs=2, batch_size=8, device='cuda')
 
     for param in tuner.embed_model.parameters():
         assert str(param.place) == 'CUDAPlace(0)'
