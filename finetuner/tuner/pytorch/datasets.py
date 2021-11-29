12bb13908d5573639ec310e1616f63ac3a8918a0
bwanglzu <bo.wang@jina.ai>
2021-11-29 16:31:54 +0100

    fix(tuner): make non-writable blob writable

--- finetuner/tuner/pytorch/datasets.py
+++ finetuner/tuner/pytorch/datasets.py
@@ -19,4 +19,8 @@ class PytorchClassDataset(ClassDataset, PytorchDataset):
 
 
 class PytorchSessionDataset(SessionDataset, PytorchDataset):
-    ...
+    def __getitem__(self, ind: int) -> Tuple['DocumentContentType', int]:
+        content, label = super().__getitem__(ind)
+        if isinstance(content, np.ndarray) and (content.flags['WRITEABLE'] == False):
+            return (deepcopy(content), label)
+        return (content, label)
