300b572aec39369dc79afbb78d7d718ca095eedd
bwanglzu <bo.wang@jina.ai>
2021-11-29 14:43:11 +0100

    fix(tuner): make copy of non writable blob

--- finetuner/tuner/pytorch/datasets.py
+++ finetuner/tuner/pytorch/datasets.py
@@ -1,10 +1,18 @@
+from typing import Tuple
+from copy import deepcopy
+
+import numpy as np
 from torch.utils.data import Dataset as PytorchDataset
 
 from ..dataset import ClassDataset, SessionDataset
 
 
 class PytorchClassDataset(ClassDataset, PytorchDataset):
-    ...
+    def __getitem__(self, ind: int) -> Tuple['DocumentContentType', int]:
+        content, label = super().__getitem__(ind)
+        if isinstance(content, np.ndarray) and (content.flags['WRITEABLE'] == False):
+            return (deepcopy(content), label)
+        return (content, label)
 
 
 class PytorchSessionDataset(SessionDataset, PytorchDataset):
