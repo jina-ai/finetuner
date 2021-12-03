5af8c22a4a386c40ff1106c7ee9f22e04b7b6576
Aziz Belaweid <azizbellaweid@hotmail.com>
2021-12-03 21:56:56 +0100

    fix: integration test

--- tests/integration/paddle/test_paddle_trainer.py
+++ tests/integration/paddle/test_paddle_trainer.py
@@ -32,10 +32,10 @@ def test_simple_sequential_model(tmpdir, params, loss):
         batch_size=params['batch_size'],
         num_items_per_class=params['num_items_per_class'],
     )
-    pt.save(model_path)
+    pt.save(path=model_path)
 
     # load the checkpoint and ensure the dim
-    user_model.set_state_dict(paddle.load(model_path))
+    user_model.set_state_dict(paddle.load(model_path)['state_dict'])
     user_model.eval()
     inputs = paddle.to_tensor(
         np.random.random(
