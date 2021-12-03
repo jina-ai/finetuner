58fd091dbf5fd9b09b72e6e8f864fa45533953ec
Aziz Belaweid <azizbellaweid@hotmail.com>
2021-12-03 10:23:34 +0100

    fix: add val loss to tuner state

--- tests/unit/tuner/paddle/test_model_checkpoint.py
+++ tests/unit/tuner/paddle/test_model_checkpoint.py
@@ -2,17 +2,27 @@ import os
 
 import pytest
 import paddle
+<<<<<<< HEAD
 import copy
 
 import finetuner
 from finetuner.tuner.callback import TrainingCheckpoint, BestModelCheckpoint
+=======
+
+import finetuner
+from finetuner.tuner.callback import ModelCheckpointCallback
+>>>>>>> fix: add val loss to tuner state
 from finetuner.tuner.base import BaseTuner
 from finetuner.toydata import generate_fashion
 from finetuner.tuner.paddle import PaddleTuner
 from finetuner.tuner.state import TunerState
 
 
+<<<<<<< HEAD
 @pytest.fixture(scope='module')
+=======
+@pytest.fixture(scope="module")
+>>>>>>> fix: add val loss to tuner state
 def paddle_model() -> BaseTuner:
     embed_model = paddle.nn.Sequential(
         paddle.nn.Flatten(),
@@ -32,6 +42,7 @@ def test_paddle_model(paddle_model: BaseTuner, tmpdir):
         epochs=1,
         train_data=generate_fashion(num_total=1000),
         eval_data=generate_fashion(is_testset=True, num_total=200),
+<<<<<<< HEAD
         callbacks=[TrainingCheckpoint(save_dir=tmpdir)],
     )
 
@@ -40,6 +51,17 @@ def test_paddle_model(paddle_model: BaseTuner, tmpdir):
 
 def test_epoch_end(paddle_model: BaseTuner, tmpdir):
     checkpoint = TrainingCheckpoint(save_dir=tmpdir)
+=======
+        callbacks=[ModelCheckpointCallback(filepath=tmpdir)],
+    )
+
+    assert os.listdir(tmpdir) == ['saved_model_epoch_01']
+    assert os.listdir(os.path.join(tmpdir, 'saved_model_epoch_01')) == ['model']
+
+
+def test_epoch_end(paddle_model: BaseTuner, tmpdir):
+    checkpoint = ModelCheckpointCallback(filepath=tmpdir, monitor="loss")
+>>>>>>> fix: add val loss to tuner state
 
     tuner = PaddleTuner(embed_model=paddle_model)
     tuner.state = TunerState(epoch=0, batch_index=2, train_loss=1.1)
@@ -47,6 +69,7 @@ def test_epoch_end(paddle_model: BaseTuner, tmpdir):
     checkpoint.on_epoch_end(tuner)
 
     assert os.listdir(tmpdir) == ['saved_model_epoch_01']
+<<<<<<< HEAD
 
 
 def test_load_model(paddle_model: BaseTuner, tmpdir):
@@ -96,3 +119,18 @@ def test_load_best_model(paddle_model: BaseTuner, tmpdir):
 
     for l1, l2 in zip(paddle_model.parameters(), new_model.parameters()):
         assert (l1 == l2).all()
+=======
+    assert os.listdir(os.path.join(tmpdir, 'saved_model_epoch_01')) == ['model']
+
+
+def test_val_end(paddle_model: BaseTuner, tmpdir):
+    checkpoint = ModelCheckpointCallback(filepath=tmpdir, monitor="val_loss")
+
+    tuner = PaddleTuner(embed_model=paddle_model)
+    tuner.state = TunerState(epoch=2, batch_index=2, val_loss=1.1)
+
+    checkpoint.on_val_end(tuner)
+
+    assert os.listdir(tmpdir) == ['saved_model_epoch_03']
+    assert os.listdir(os.path.join(tmpdir, 'saved_model_epoch_03')) == ['model']
+>>>>>>> fix: add val loss to tuner state
