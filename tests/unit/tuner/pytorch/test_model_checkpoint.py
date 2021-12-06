6cba81eaa7abb8f2f001c570265ce501f32d1695
Aziz Belaweid <azizbellaweid@hotmail.com>
2021-12-06 16:14:28 +0100

    fix: fix failing tests

--- tests/unit/tuner/pytorch/test_model_checkpoint.py
+++ tests/unit/tuner/pytorch/test_model_checkpoint.py
@@ -102,7 +102,7 @@ def test_load_best_model(pytorch_model: BaseTuner, tmpdir):
         epochs=1,
         train_data=generate_fashion(num_total=1000),
         eval_data=generate_fashion(is_testset=True, num_total=200),
-        callbacks=[TrainingModelCheckpoint(save_dir=tmpdir)],
+        callbacks=[BestModelCheckpoint(save_dir=tmpdir)],
     )
 
     checkpoint = torch.load(os.path.join(tmpdir, 'best_model_val_loss'))
