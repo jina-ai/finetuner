bd12c08b5e6a98e23f12e576d6744edc3a41988e
yanlong.wang <yanlong.wang@naiver.org>
2021-12-17 19:30:20 +0800

    fix(docs): <jina-qa-bot> => <qa-bot>

--- docs/conf.py
+++ docs/conf.py
@@ -188,7 +188,7 @@ ogp_custom_meta_tags = [
 </script>
 <!-- Place this tag in your head or just before your close body tag. -->
 <script async defer src="https://buttons.github.io/buttons.js"></script>
-<script async defer src="https://cdn.jsdelivr.net/npm/@jina-ai/jina-qa-bot@0.1.1"></script>
+<script async defer src="https://cdn.jsdelivr.net/npm/qabot@0.1.1"></script>
     ''',
 ]
 
@@ -204,11 +204,11 @@ smv_remote_whitelist = None
 
 
 def set_qa_server_address(app):
-    # This sets the server address to <jina-qa-bot>
+    # This sets the server address to <qa-bot>
     server_address = app.config['server_address']
     js_text = """
         document.addEventListener("DOMContentLoaded", function() { 
-            document.querySelector("jina-qa-bot").setAttribute("server", "%s");
+            document.querySelector("qa-bot").setAttribute("server", "%s");
         });
         """ % server_address
     app.add_js_file(None, body=js_text)
