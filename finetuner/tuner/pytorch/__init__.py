5da894d9607864be119d6f2c7aa4b02bf83c52b6
bwanglzu <bo.wang@jina.ai>
2022-01-04 14:09:46 +0100

    feat(tuner): add default projection head for ssl

--- finetuner/tuner/pytorch/__init__.py
+++ finetuner/tuner/pytorch/__init__.py
@@ -84,6 +84,9 @@ class PytorchTuner(BaseTuner[nn.Module, DataLoader, Optimizer, _LRScheduler]):
         self.device = get_device(self._device_name)
         self._embed_model = self._embed_model.to(self.device)
 
+    def _attach_projection_head(self, dim: int = 128):
+        pass
+
     def _default_configure_optimizer(self, model: nn.Module) -> Optimizer:
         """Get the default Adam optimizer"""
         optimizer = torch.optim.Adam(model.parameters(), lr=self._learning_rate)
@@ -226,3 +229,37 @@ def get_device(device: str):
 
     # translate our own alias into framework-compatible ones
     return torch.device(device)
+
+
+class _ProjectionHead(nn.Module):
+    """Projection head used internally for self-supervised training.
+    It is (by default) a simple 3-layer MLP to be attached on top of embedding model only for training purpose.
+    After training, it should be cut-out from the embedding model.
+    """
+
+    EPSILON = 1e-5
+
+    def __init__(self, in_features: int, output_dim: int = 128, num_layers: int = 3):
+        super().__init__()
+        self.head_layers = nn.ModuleList()
+        is_last_layer = False
+        out_features = in_features
+        for idx in range(num_layers):
+            if idx == num_layers - 1:
+                is_last_layer = True
+                out_features = output_dim
+            self.head_layers.append(
+                nn.Linear(
+                    in_features=in_features, out_features=out_features, bias=False
+                )
+            )
+            if not is_last_layer:
+                self.head_layers.append(
+                    nn.BatchNorm1d(num_features=out_features, eps=self.EPSILON)
+                )
+                self.head_layers.append(nn.ReLU())
+
+    def forward(self, x):
+        for layer in self.head_layers:
+            x = layer(x)
+        return x
