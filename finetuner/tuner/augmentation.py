fix: add normalization to preprocessor (#340)

finetuner/tuner/augmentation.py
@@ -7,6 +7,7 @@ def vision_preprocessor(
     width: int = 224,
     default_channel_axis: int = -1,
     target_channel_axis: int = 0,
+    phase: str = 'train',
 ):
     """Randomly augmentation a Document with `blob` field.
     The method applies flipping, color jitter, cropping, gaussian blur and random rectangle erase
@@ -16,11 +17,13 @@ def vision_preprocessor(
     :param width: image width.
     :param default_channel_axis: The color channel of the input image, by default -1, the expected input is H, W, C.
     :param target_channel_axis: The color channel of the output image, by default 0, the expected output is C, H, W.
+    :param phase: phase of experiment, either `train` or `validation`. At `validation` phase, will not apply
+      random transformation.
     """
 
     def preprocess_fn(doc):
         return _vision_preprocessor(
-            doc, height, width, default_channel_axis, target_channel_axis
+            doc, height, width, default_channel_axis, target_channel_axis, phase
         )
 
     return preprocess_fn
@@ -32,6 +35,7 @@ def _vision_preprocessor(
     width: int = 224,
     default_channel_axis: int = -1,
     target_channel_axis: int = 0,
+    phase: str = 'train',
 ):
     """Randomly augmentation a Document with `blob` field.
     The method applies flipping, color jitter, cropping, gaussian blur and random rectangle erase
@@ -42,6 +46,8 @@ def _vision_preprocessor(
     :param width: image width.
     :param default_channel_axis: The color channel of the input image, by default -1, the expected input is H, W, C.
     :param target_channel_axis: The color channel of the output image, by default 0, the expected output is C, H, W.
+    :param phase: stage of experiment, either `train` or `validation`. At `validation` phase, will not apply
+        random transformation.
     """
     import albumentations as A
 
@@ -54,20 +60,34 @@ def _vision_preprocessor(
             )
             blob = doc.blob
         else:
-            raise AttributeError('Can not load `blob` field from the given document.')
+            raise AttributeError(
+                f'Document `blob` is None, loading it from url: {doc.uri} failed.'
+            )
+    if blob.dtype == np.float64:
+        blob = np.float32(blob)
     if default_channel_axis not in [-1, 2]:
         blob = np.moveaxis(blob, default_channel_axis, -1)
-    # p is the probability to apply the transform.
-    transform = A.Compose(
-        [
-            A.HorizontalFlip(p=0.5),
-            A.ColorJitter(p=1, brightness=0, contrast=0, saturation=0, hue=0),
-            A.RandomResizedCrop(width=width, height=height, p=1),
-            A.GaussianBlur(p=1),
-            A.GridDropout(p=0.5),
-        ]
-    )
-    blob = transform(image=blob)['image']
+    if blob.shape[-1] == 3:  # apply to RGB images.
+        transform = A.Compose(
+            [
+                A.Normalize(),
+                A.ToFloat(),
+            ]
+        )
+        blob = transform(image=blob)['image']
+    if phase == 'train':
+        transform = A.Compose(
+            [
+                A.HorizontalFlip(p=0.5),
+                A.ColorJitter(p=1),
+                A.RandomResizedCrop(width=width, height=height, p=1),
+                A.GaussianBlur(p=1),
+                A.GridDropout(
+                    ratio=0.2, p=0.5
+                ),  # random erase 0.2 percent of image with 0.5 probability
+            ]
+        )
+        blob = transform(image=blob)['image']
     if target_channel_axis != -1:
         blob = np.moveaxis(blob, -1, target_channel_axis)
     return blob

tests/unit/tuner/test_augmentation.py
@@ -9,11 +9,20 @@ from finetuner.tuner.augmentation import _vision_preprocessor
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
+@pytest.mark.parametrize('phase', ['train', 'validation'])
 @pytest.mark.parametrize(
     'doc, height, width, num_channels, default_channel_axis, target_channel_axis',
     [
         (Document(blob=np.random.rand(224, 224, 3)), 224, 224, 3, -1, 0),
         (Document(blob=np.random.rand(256, 256, 3)), 256, 256, 3, -1, 0),
+        (
+            Document(blob=np.random.rand(256, 256, 3).astype('uint8')),
+            256,
+            256,
+            3,
+            -1,
+            0,
+        ),
         (Document(blob=np.random.rand(256, 256, 1)), 256, 256, 1, -1, 0),  # grayscale
         (
             Document(blob=np.random.rand(256, 256, 3)),
@@ -42,18 +51,19 @@ cur_dir = os.path.dirname(os.path.abspath(__file__))
     ],
 )
 def test_vision_preprocessor(
-    doc, height, width, num_channels, default_channel_axis, target_channel_axis
+    doc, height, width, num_channels, default_channel_axis, target_channel_axis, phase
 ):
     original_blob = doc.blob
     augmented_blob = _vision_preprocessor(
-        doc, height, width, default_channel_axis, target_channel_axis
+        doc, height, width, default_channel_axis, target_channel_axis, phase
     )
     assert augmented_blob is not None
+    assert not np.array_equal(original_blob, augmented_blob)
+    assert np.issubdtype(augmented_blob.dtype, np.floating)
     if target_channel_axis == -1:
         assert augmented_blob.shape == (height, width, num_channels)
     elif target_channel_axis == 0:
         assert augmented_blob.shape == (num_channels, height, width)
-    assert not np.array_equal(original_blob, augmented_blob)
 
 
 def test_vision_preprocessor_fail_given_no_blob_and_uri():