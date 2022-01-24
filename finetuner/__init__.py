c82c4f1c1d577e8c7f5b4124c465f41f29509e72
Tadej Svetina <tadej.svetina@gmail.com>
2022-01-24 13:11:53 +0100

    feat: drop labeler

--- finetuner/__init__.py
+++ finetuner/__init__.py
@@ -92,85 +92,13 @@ def fit(
     ...
 
 
-# fit interface from Labeler + Tuner
-@overload
-def fit(
-    model: 'AnyDNN',  #: must be an embedding model
-    train_data: 'DocumentArray',
-    eval_data: Optional['DocumentArray'] = None,
-    epochs: int = 10,
-    batch_size: int = 256,
-    loss: Union[str, 'AnyDNN'] = 'SiameseLoss',
-    configure_optimizer: Optional[
-        Callable[
-            ['AnyDNN'], Union['AnyOptimizer', Tuple['AnyOptimizer', 'AnyScheduler']]
-        ]
-    ] = None,
-    learning_rate: float = 1e-3,
-    scheduler_step: str = 'batch',
-    device: str = 'cpu',
-    preprocess_fn: Optional['PreprocFnType'] = None,
-    collate_fn: Optional['CollateFnType'] = None,
-    num_items_per_class: Optional[int] = None,
-    callbacks: Optional[List['BaseCallback']] = None,
-    num_workers: int = 0,
-    interactive: bool = True,  #: below are labeler args
-    clear_labels_on_start: bool = False,
-    port_expose: Optional[int] = None,
-    runtime_backend: str = 'thread',
-) -> 'AnyDNN':
-    ...
-
-
-# fit interface from Labeler + Tailor + Tuner
-@overload
-def fit(
-    model: 'AnyDNN',
-    train_data: 'DocumentArray',
-    eval_data: Optional['DocumentArray'] = None,
-    epochs: int = 10,
-    batch_size: int = 256,
-    loss: Union[str, 'AnyDNN'] = 'SiameseLoss',
-    configure_optimizer: Optional[
-        Callable[
-            ['AnyDNN'], Union['AnyOptimizer', Tuple['AnyOptimizer', 'AnyScheduler']]
-        ]
-    ] = None,
-    learning_rate: float = 1e-3,
-    scheduler_step: str = 'batch',
-    device: str = 'cpu',
-    preprocess_fn: Optional['PreprocFnType'] = None,
-    collate_fn: Optional['CollateFnType'] = None,
-    num_items_per_class: Optional[int] = None,
-    callbacks: Optional[List['BaseCallback']] = None,
-    num_workers: int = 0,
-    interactive: bool = True,  #: below are labeler args
-    clear_labels_on_start: bool = False,
-    port_expose: Optional[int] = None,
-    runtime_backend: str = 'thread',
-    to_embedding_model: bool = True,  #: below are tailor args
-    input_size: Optional[Tuple[int, ...]] = None,
-    input_dtype: str = 'float32',
-    layer_name: Optional[str] = None,
-    freeze: Union[bool, List[str]] = False,
-    projection_head: Optional['AnyDNN'] = None,
-) -> 'AnyDNN':
-    ...
-
-
 def fit(model: 'AnyDNN', train_data: 'DocumentArray', *args, **kwargs) -> 'AnyDNN':
     if kwargs.get('to_embedding_model', False):
         from .tailor import to_embedding_model
 
         model = to_embedding_model(model, *args, **kwargs)
 
-    if kwargs.get('interactive', False):
-        from .labeler import fit
+    from .tuner import fit
 
-        fit(model, train_data, *args, **kwargs)
-        return model
-    else:
-        from .tuner import fit
-
-        fit(model, train_data, *args, **kwargs)
-        return model
+    fit(model, train_data, *args, **kwargs)
+    return model
