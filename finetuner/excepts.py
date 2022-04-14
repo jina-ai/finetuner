f5efab77bd7b31869827c661421a5247674cdee6
jupyterjazz <saba.sturua@jina.ai>
2022-04-14 18:32:02 +0400

    refactor: setup a project

--- finetuner/excepts.py
+++
@@ -1,9 +0,0 @@
-"""This modules defines all kinds of exceptions raised in Finetuner."""
-
-
-class DimensionMismatchException(Exception):
-    """Dimensionality mismatch given input and output layers."""
-
-
-class DeviceError(Exception):
-    """Model is placed on the wrong device."""
