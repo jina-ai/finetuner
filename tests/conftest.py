e59e9e0227c3feb407b2caaec700c12146f9a299
jupyterjazz <saba.sturua@jina.ai>
2022-04-22 11:44:23 +0200

    refactor: modify a fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,11 @@ import pytest
 from finetuner import Client
 
 
-def mock_handle_request(self, **kwargs):
-    return kwargs
-
-
 @pytest.fixture
 def test_client(mocker):
+    def mock_handle_request(self, **kwargs):
+        return kwargs
+
     mocker.patch.object(Client, 'handle_request', mock_handle_request)
     client = Client(user_id=1)
     return client
