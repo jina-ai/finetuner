68d267e3d63e68803abb198ff0eaafbf513278f5
jupyterjazz <saba.sturua@jina.ai>
2022-05-12 01:45:43 +0400

    chore: remove unused test

--- finetuner/classes/__init__.py
+++ finetuner/classes/__init__.py
@@ -1,2 +1,2 @@
-from finetuner.classes.experiment import Experiment
-from finetuner.classes.run import Run
+from finetuner.classes.experiment import Experiment # noqa
+from finetuner.classes.run import Run # noqa
