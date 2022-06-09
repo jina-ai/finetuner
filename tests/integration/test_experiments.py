5ed67ec44167feff1f9e526844c989c317165531
bwanglzu <bo.wang@jina.ai>
2022-06-09 15:57:34 +0200

    test: do not assert num experiments

--- tests/integration/test_experiments.py
+++ tests/integration/test_experiments.py
@@ -4,8 +4,6 @@ from tests.helper import create_random_name
 def test_experiments(
     finetuner_mocker,
 ):
-    # Always has one default Experiment.
-    assert len(finetuner_mocker.list_experiments()) == 1
     first_exp_name, second_exp_name = [create_random_name() for _ in range(2)]
     # create an experiment and retrieve it
     finetuner_mocker.create_experiment(name=first_exp_name)
@@ -15,7 +13,6 @@ def test_experiments(
     # create another experiment and list all experiments
     finetuner_mocker.create_experiment(second_exp_name)
     experiments = finetuner_mocker.list_experiments()
-    assert len(experiments) == 3
     experiment_names = [experiment.name for experiment in experiments]
     assert first_exp_name in experiment_names and second_exp_name in experiment_names
 
@@ -24,7 +21,6 @@ def test_experiments(
     # delete the first experiment
     finetuner_mocker.delete_experiment(first_exp_name)
     experiments = finetuner_mocker.list_experiments()
-    assert len(experiments) == 2
     assert second_exp_name in [experiment.name for experiment in experiments]
     # delete all experiments
     finetuner_mocker.delete_experiment(second_exp_name)
