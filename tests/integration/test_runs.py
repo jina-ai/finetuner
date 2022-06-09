e00efa179baf9c327cc791baf804b8e971360bf7
bwanglzu <bo.wang@jina.ai>
2022-06-09 12:49:23 +0200

    test: remove unused test

--- tests/integration/test_runs.py
+++ tests/integration/test_runs.py
@@ -14,9 +14,9 @@ def test_runs(
     train_data, eval_data = get_image_data
     # create an experiment and retrieve it
     finetuner_mocker.create_experiment(experiment_name)
-    exp = finetuner_mocker.get_experiment(name=experiment_name)
-    assert exp.name == experiment_name
-    assert exp.status == 'ACTIVE'
+    experiment = finetuner_mocker.get_experiment(name=experiment_name)
+    assert experiment.name == experiment_name
+    assert experiment.status == 'ACTIVE'
     # Create Runs
     first_run, second_run = [create_random_name(prefix='run') for _ in range(2)]
     # create a first run
@@ -58,72 +58,29 @@ def test_runs(
     assert not experiments
 
 
-def test_list_runs(
-    finetuner_mocker,
-    get_image_data,
-):
-    first_exp, second_exp = [create_random_name() for _ in range(2)]
-    first_run, second_run = [create_random_name(prefix='run') for _ in range(2)]
-    # get preprocessed data
-    train_data, eval_data = get_image_data
-    # create two experiments and list them
-    finetuner_mocker.create_experiment(name=first_exp)
-    finetuner_mocker.create_experiment(name=second_exp)
-    experiments = finetuner_mocker.list_experiments()
-    experiment_names = [experiment.name for experiment in experiments]
-    assert first_exp and second_exp in experiment_names
-    assert len(experiment_names) == 3
-    # create a run for each experiment
-    finetuner_mocker.create_run(
+def test_create_run_and_save_model(finetuner_mocker, get_image_data, tmp_path):
+    import time
+
+    train_da, _ = get_image_data
+    experiment_name = create_random_name()
+    finetuner_mocker.create_experiment(name=experiment_name)
+    run = finetuner_mocker.create_run(
         model='resnet50',
-        train_data=train_data,
-        eval_data=eval_data,
-        experiment_name=first_exp,
-        run_name=first_run,
-        epochs=1,
+        train_data=train_da,
+        loss='TripletMarginLoss',
+        optimizer='Adam',
+        learning_rate=0.001,
+        batch_size=10,
+        epochs=2,
+        run_name=create_random_name(prefix='run'),
+        experiment_name=experiment_name,
     )
-    finetuner_mocker.create_run(
-        model='resnet50',
-        train_data=train_data,
-        eval_data=eval_data,
-        experiment_name=second_exp,
-        run_name=second_run,
-        epochs=1,
-    )
-    # list all runs without specifying a target experiment
-    # which should list all runs across all existing experiments
-    runs = finetuner_mocker.list_runs()
-    assert len(runs) == 2
-    run_names = [run.name for run in runs]
-    assert first_run and second_run in run_names
-    # list all runs of only first experiment
-    runs = finetuner_mocker.list_runs(experiment_name=first_exp)
-    assert len(runs) == 1
-    # delete experiments
+    while run.status()[STATUS] not in [FINISHED, FAILED]:
+        time.sleep(3)
+    assert run.status()[STATUS] == FINISHED
+    run.save_model(path=tmp_path / 'finetuned_model')
+    assert os.path.exists(tmp_path / 'finetuned_model')
+    # delete all experiments (and runs)
     finetuner_mocker.delete_experiments()
     experiments = finetuner_mocker.list_experiments()
     assert not experiments
-
-
-# def test_create_run_and_save_model(finetuner_mocker, get_image_data, tmp_path):
-#     import time
-#
-#     train_da, _ = get_image_data
-#     run = finetuner_mocker.create_run(
-#         model='resnet50',
-#         train_data=train_da,
-#         loss='TripletMarginLoss',
-#         optimizer='Adam',
-#         learning_rate=0.001,
-#         batch_size=10,
-#         epochs=2,
-#     )
-#     while run.status()[STATUS] not in [FINISHED, FAILED]:
-#         time.sleep(3)
-#     assert run.status()[STATUS] == FINISHED
-#     run.save_model(path=tmp_path / 'finetuned_model')
-#     assert os.path.exists(tmp_path / 'finetuned_model')
-#     # delete all experiments (and runs)
-#     finetuner_mocker.delete_experiments()
-#     exps = finetuner_mocker.list_experiments()
-#     assert not exps
