2d1b0f525933cf7563d709536d9f8aa3935192ce
bwanglzu <bo.wang@jina.ai>
2022-06-09 12:08:09 +0200

    refactor: fix test list runs

--- tests/integration/test_runs.py
+++ tests/integration/test_runs.py
@@ -62,8 +62,8 @@ def test_list_runs(
     finetuner_mocker,
     get_image_data,
 ):
-    first_exp, second_exp = create_random_name()
-    first_run, second_run = create_random_name(prefix='run')
+    first_exp, second_exp = [create_random_name() for _ in range(2)]
+    first_run, second_run = [create_random_name(prefix='run') for _ in range(2)]
     # get preprocessed data
     train_data, eval_data = get_image_data
     # create two experiments and list them
@@ -72,7 +72,7 @@ def test_list_runs(
     experiments = finetuner_mocker.list_experiments()
     experiment_names = [experiment.name for experiment in experiments]
     assert first_exp and second_exp in experiment_names
-    assert len(experiment_names) == 2
+    assert len(experiment_names) == 3
     # create a run for each experiment
     finetuner_mocker.create_run(
         model='resnet50',
@@ -82,28 +82,27 @@ def test_list_runs(
         run_name=first_run,
         epochs=1,
     )
-    # finetuner_mocker.create_run(
-    #     model='resnet50',
-    #     train_data=train_data,
-    #     eval_data=eval_data,
-    #     experiment_name=second_exp,
-    #     run_name=second_run,
-    #     epochs=1,
-    # )
-    # # list all runs without specifying a target experiment
-    # # which should list all runs across all existing experiments
-    # runs = finetuner_mocker.list_runs()
-    # assert len(runs) == 2
-    # run_names = sorted([run.name for run in runs])
-    # assert run_names == [first_run, second_run]
-    # # list all runs of only first experiment
-    # runs = finetuner_mocker.list_runs(experiment_name=first_exp)
-    # assert len(runs) == 1
-    # assert runs[0].name == first_run
-    # # delete experiments
-    # finetuner_mocker.delete_experiments()
-    # exps = finetuner_mocker.list_experiments()
-    # assert not exps
+    finetuner_mocker.create_run(
+        model='resnet50',
+        train_data=train_data,
+        eval_data=eval_data,
+        experiment_name=second_exp,
+        run_name=second_run,
+        epochs=1,
+    )
+    # list all runs without specifying a target experiment
+    # which should list all runs across all existing experiments
+    runs = finetuner_mocker.list_runs()
+    assert len(runs) == 2
+    run_names = [run.name for run in runs]
+    assert first_run and second_run in run_names
+    # list all runs of only first experiment
+    runs = finetuner_mocker.list_runs(experiment_name=first_exp)
+    assert len(runs) == 1
+    # delete experiments
+    finetuner_mocker.delete_experiments()
+    experiments = finetuner_mocker.list_experiments()
+    assert not experiments
 
 
 # def test_create_run_and_save_model(finetuner_mocker, get_image_data, tmp_path):
