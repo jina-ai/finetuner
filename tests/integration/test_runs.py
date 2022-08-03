a0cd679ddf5b7b64353a709ae1906e1aca5ac803
bwanglzu <bo.wang@jina.ai>
2022-08-03 10:46:02 +0200

    test: enable integration test

--- tests/integration/test_runs.py
+++ tests/integration/test_runs.py
@@ -1,8 +1,8 @@
-# import os
+import os
 
 from tests.helper import create_random_name
 
-# from finetuner.constants import FAILED, FINISHED, STATUS
+from finetuner.constants import FAILED, FINISHED, STATUS
 
 
 def test_runs(finetuner_mocker, get_feature_data):
@@ -77,39 +77,39 @@ def test_runs(finetuner_mocker, get_feature_data):
     assert experiment_name not in [experiment.name for experiment in experiments]
 
 
-# def test_create_run_and_save_model(finetuner_mocker, get_feature_data, tmp_path):
-#     import time
-#
-#     train_da, _ = get_feature_data
-#     experiment_name = create_random_name()
-#     finetuner_mocker.create_experiment(name=experiment_name)
-#     run = finetuner_mocker.create_run(
-#         model='mlp',
-#         model_options={'input_size': 128, 'hidden_sizes': [32]},
-#         train_data=train_da,
-#         loss='TripletMarginLoss',
-#         optimizer='Adam',
-#         learning_rate=0.001,
-#         batch_size=10,
-#         epochs=2,
-#         experiment_name=experiment_name,
-#     )
-#     status = run.status()[STATUS]
-#     while status not in [FAILED, FINISHED]:
-#         time.sleep(10)
-#         status = run.status()[STATUS]
-#
-#     assert status == FINISHED
-#
-#     artifact_id = run.artifact_id
-#     assert isinstance(artifact_id, str)
-#     # the artifact id is a 24 character hex string defined in mongo db.
-#     assert len(artifact_id) == 24
-#
-#     run.save_artifact(directory=tmp_path / 'finetuned_model')
-#     assert os.path.exists(tmp_path / 'finetuned_model')
-#
-#     # delete created experiments (and runs)
-#     finetuner_mocker.delete_experiment(experiment_name)
-#     experiments = finetuner_mocker.list_experiments()
-#     assert experiment_name not in [experiment.name for experiment in experiments]
+def test_create_run_and_save_model(finetuner_mocker, get_feature_data, tmp_path):
+    import time
+
+    train_da, _ = get_feature_data
+    experiment_name = create_random_name()
+    finetuner_mocker.create_experiment(name=experiment_name)
+    run = finetuner_mocker.create_run(
+        model='mlp',
+        model_options={'input_size': 128, 'hidden_sizes': [32]},
+        train_data=train_da,
+        loss='TripletMarginLoss',
+        optimizer='Adam',
+        learning_rate=0.001,
+        batch_size=10,
+        epochs=2,
+        experiment_name=experiment_name,
+    )
+    status = run.status()[STATUS]
+    while status not in [FAILED, FINISHED]:
+        time.sleep(10)
+        status = run.status()[STATUS]
+
+    assert status == FINISHED
+
+    artifact_id = run.artifact_id
+    assert isinstance(artifact_id, str)
+    # the artifact id is a 24 character hex string defined in mongo db.
+    assert len(artifact_id) == 24
+
+    run.save_artifact(directory=tmp_path / 'finetuned_model')
+    assert os.path.exists(tmp_path / 'finetuned_model')
+
+    # delete created experiments (and runs)
+    finetuner_mocker.delete_experiment(experiment_name)
+    experiments = finetuner_mocker.list_experiments()
+    assert experiment_name not in [experiment.name for experiment in experiments]
