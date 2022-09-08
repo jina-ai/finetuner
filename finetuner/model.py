8a0f326ddf3ca92d3b799e727a89a27d80450143
bwanglzu <bo.wang@jina.ai>
2022-09-08 13:54:07 +0200

    refactor: adopt changes in open clip models

--- finetuner/model.py
+++ finetuner/model.py
@@ -9,7 +9,7 @@ def get_header() -> Tuple[str, ...]:
 def get_row(model_stub) -> Tuple[str, ...]:
     """Get table row."""
     return (
-        model_stub.name,
+        model_stub.descriptor,
         model_stub.task,
         str(model_stub.output_shape[1]),
         model_stub.architecture,
