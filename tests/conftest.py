731318760b7682bdf9fa4013d94cf86a8f33fdc1
bwanglzu <bo.wang@jina.ai>
2022-09-09 10:13:27 +0200

    fix: log streaming and staging env

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def overwrite_hubble_registry():
-    os.environ['JINA_FINETUNER_REGISTRY'] = 'https://api.finetuner.fit'
-    os.environ['JINA_HUBBLE_REGISTRY'] = 'https://api.hubble.jina.ai'
+    os.environ['JINA_FINETUNER_REGISTRY'] = 'https://api.staging.finetuner.fit'
+    os.environ['JINA_HUBBLE_REGISTRY'] = 'https://apihubble.staging.jina.ai'
     yield
     del os.environ['JINA_HUBBLE_REGISTRY']
     del os.environ['JINA_FINETUNER_REGISTRY']
