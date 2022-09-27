2b229cd5e4f05353d7256d7ac7afdcfd64436689
Michael Günther <guenthermi50@gmail.com>
2022-09-27 17:07:02 +0200

    Fix log messages (#549)

--- finetuner/model.py
+++ finetuner/model.py
@@ -1,3 +1,6 @@
+import inspect
+
+from stubs import model
 from stubs.model import *  # noqa F401
 
 
@@ -15,3 +18,12 @@ def get_row(model_stub) -> Tuple[str, ...]:
         model_stub.architecture,
         model_stub.description,
     )
+
+
+def list_model_classes() -> Dict[str, ModelStubType]:
+    rv = {}
+    members = inspect.getmembers(model, inspect.isclass)
+    for name, stub in members:
+        if name != 'MLPStub' and not name.startswith('_') and type(stub) != type:
+            rv[name] = stub
+    return rv
