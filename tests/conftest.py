0346c9538ff850c5e53d8537b96d12e86a91c105
guenthermi <guenthermi50@gmail.com>
2023-02-13 11:44:07 +0100

    chore: change url of staging environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def overwrite_hubble_registry():
-    os.environ['JINA_FINETUNER_REGISTRY'] = 'https://api-staging.finetuner.fit'
+    os.environ['JINA_FINETUNER_REGISTRY'] = 'https://api.staging.finetuner.fit'
     os.environ['JINA_HUBBLE_REGISTRY'] = 'https://api.hubble.jina.ai'
     yield
     del os.environ['JINA_HUBBLE_REGISTRY']
