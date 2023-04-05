d2fad3c7bb776dac81995c57f4a26a8d62d1c924
lmmilliken <louis.milliken@jina.ai>
2023-04-05 09:02:13 +0200

    feat: add error message when experiment is

--- finetuner/finetuner.py
+++ finetuner/finetuner.py
@@ -185,6 +185,13 @@ class Finetuner:
             experiment = self._default_experiment
         else:
             experiment = self.get_experiment(name=experiment_name)
+        if not experiment:
+            raise ValueError(
+                (
+                    'unable to start finetuning run as experiment is `None`. '
+                    'make sure you have logged in using `finetuner.login()`.'
+                )
+            )
         return experiment.create_run(
             model=model,
             train_data=train_data,
